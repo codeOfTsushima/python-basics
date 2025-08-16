f0663bb65724c256659b0508ca27490d19f6559f
Parthip <parthip.mp303@gmail.com>
2025-08-16 21:36:05 +0530

    Updated randomNumber

--- randomNumber.py
+++ randomNumber.py
@@ -2,8 +2,8 @@ import random
 num = random.randint(1, 10)
 guess = int(input("guess a number between 1 and 10"))
 if guess == num:
-    print("correct, You guessed it....yayyyy")
+    print("correct, You guessed it right!")
 elif guess < num:
-    print("too low noob, The number was", num)
+    print("too low, The number was", num)
 else:
-    print("too high noob, The number was", num)
+    print("too high, The number was", num)
