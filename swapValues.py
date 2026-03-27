aefe58c3197b44f26d179eef97bd2c93a48fd203
codeOfTsushima <parthip.mp303@gmail.com>
2026-03-27 22:08:01 +0530

    Added swapvalues

---
+++ swapValues.py
@@ -0,0 +1,10 @@
+N =  int(input())
+F = int(input())
+
+print("Initial salaries")
+print(f"Alice's salary = {N}")
+print(f"Bob's salary = {F}")
+N,F
+print("new salary after swapping")
+print(f"Alice's salary = {F}")
+print(f"Bob's salary = {N}")
